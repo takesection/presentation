d4bbb6f55e0351596caef5511ecd6a87e9265c1c
Shigeki Shoji <takesection@gmail.com>
2022-09-18 01:40:32 +0900

    test

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const readFile = fs.readFile;
+const writeFile = fs.writeFile;
+const markdownItIframe = require('markdown-it-iframe');
+
+var md = require('markdown-it')().use(markdownItIframe, {
+    width: 976,
+    height: 549,
+    frameborder: 1
+});
+
+readFile('index.md', (err, data) => {
+    console.log(data);
+    if (err) throw err;
+    writeFile('introduction-of-functional-programming.html', md.render(data.toString()), (err) => {
+        if (err) throw err;
+    });
+});
---
+++ index.md
@@ -0,0 +1,4 @@
+関数型プログラミング超入門
+===
+
+/i/https:src/introduction-of-functional-programming.html
