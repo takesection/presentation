0824f52d2b6ab7d0e5ce7e5fa12350f6ede8293e
Shigeki Shoji <takesection@gmail.com>
2022-09-18 21:44:21 +0900

    fix eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,23 @@
-const marpCore = require("@marp-team/marp-core")
+const crypto = require("node:crypto");
+const fs = require("node:fs");
+const marp = require("@marp-team/marp-cli");
 
 module.exports = eleventyConfig => {
-    /*
     eleventyConfig.addTemplateFormats("marp");
     eleventyConfig.addExtension("marp", {
-        compile: async (inputContent) => {
-            const marp = new marpCore.Marp({
-                html: true,
-                minifyCSS: true,
-                script: {
-                    source: 'cdn'
+        compile: async (inputContent, inputPath) => {
+            const outputPath = crypto.randomUUID() + '.html';
+            return marp.marpCli([ inputPath, "-o", outputPath]).then(exitCode => {
+                if (exitCode !== 0) throw "Error";
+                try {
+                    const result = fs.readFileSync(outputPath);
+                    return () => result;
+                } finally {
+                    fs.rmSync(outputPath);
                 }
             });
-            const { html, css } = marp.render(inputContent);
-            console.log(html);
-            console.log(css);
-            return async () => {
-                return "<!DOCTYPE html>\n<html>\n<head>\n<style>" + css + "\n</style>\n</head>\n<body>\n" + html + "\n</body>\n</html>";
-            };
         }
     });
-    */
     return {
         dir: {
             input: "src",
