ec4ed1e2c679f97af89cc3b60690cf51c64af796
Shigeki Shoji <takesection@gmail.com>
2022-09-22 11:40:19 +0900

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,46 @@
-const marpCore = require("@marp-team/marp-core");
-const marpit = require("@marp-team/marpit");
 const fs = require("fs");
+const { Marp } = require("@marp-team/marp-core");
+const { Element } = require("@marp-team/marpit");
+const { DateTime } = require("luxon");
+const { Liquid } = require("liquidjs");
 
-module.exports = eleventyConfig => {
+module.exports = function(eleventyConfig) {
+    const marp = new Marp({
+        container: [
+            new Element('div', { id: 'p'})
+        ]
+    });
+
+    const liquid = new Liquid({}); 
+
+    const dateFormat = (jsDate) => 
+        DateTime.fromJSDate(jsDate).setLocale('en').toLocaleString({ year: 'numeric', month: 'long', day: 'numeric' });
+   
+
     eleventyConfig.addPassthroughCopy("src/script");
     eleventyConfig.addPassthroughCopy("src/img");
+    
     eleventyConfig.addTemplateFormats("marp");
     eleventyConfig.addExtension("marp", {
+        compileOptions: {
+            permalink: async (inputContent, inputPath) => {
+                const tmpl = liquid.parse(inputContent, inputPath);
+                return async (data) => {
+                    return liquid.render(tmpl, data, {});
+                }
+            }
+        },
         compile: async (inputContent, inputPath) => {
-            const marp = new marpCore.Marp({
-                container: [
-                    new marpit.Element('div', { id: 'p'})
-                ]
-            });
-            const content = fs.readFileSync(inputPath).toString();
-            const result = marp.render(content);
+            const result = marp.render(fs.readFileSync(inputPath).toString());
             return async (data) => {
                 data.css = result.css;
                 return result.html;
             }; 
         }
     });
+
+    eleventyConfig.addFilter("dateFormat", dateFormat);
+
     return {
         dir: {
             input: "src",
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+title: Demo
+---
+# デモサイト
+
+<iframe src="presentation/2022/09/14/introduction-of-functional-programming/" width="832px" height="468px" class="slide"></iframe>
+
+[GitHub リポジトリ](https://github.com/takesection/presentation)
